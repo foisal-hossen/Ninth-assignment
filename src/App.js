3a4b6229a5d4e6430112edd8df8dff89d6e77d45
foisal-hossen <farjana01811389904@gmail.com>
2022-10-11 16:21:44 +0600

    component section added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import logo from './logo.svg';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 
 function App() {
+  const router = createBrowserRouter([
+    
+  ])
   return (
     <div className="App">
-      
+      <RouterProvider router ={router}></RouterProvider>
     </div>
   );
 }
